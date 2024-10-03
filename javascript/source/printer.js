8de4cb2a51a9643cac987050a08965a21ebf499c
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-03 22:12:15 +0900

    ✨: 입력 변수 세팅

--- javascript/source/printer.js
+++ javascript/source/printer.js
@@ -1,6 +1,11 @@
 let path = require('path');
 let inputPath = path.join(__dirname, '/dev/stdin'); // __dirname은 현재 스크립트의 디렉토리 절대경로
-let input = require('fs').readFileSync(inputPath).toString().trim().split('\r\n');
+let input = require('fs').readFileSync(inputPath).toString().trim().split('\n');
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');  //백준 제출
 
-console.log('Hello, World!');
+let currentTime = input[0].split(" ");
+let hour = parseInt(currentTime[0]);
+let min = parseInt(currentTime[1]);
+let cookTime = parseInt(input[1]);
+
+console.log(hour, min, cookTime);
